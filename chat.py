2c18e1d42833977030494b491845c522f83077b8
Kyle Delaney <kyle_delaney@msn.com>
2024-02-15 11:24:30 +0400

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import time
+
+if "pending" not in st.session_state:
+    st.session_state.history = 'Bot: Hello user'
+    st.session_state.message = None
+    st.session_state.pending = False
+
+def generate_reply(message_to_bot):
+    # Bot logic goes here
+    reply = f'You said, "{message_to_bot}"'
+    return reply
+
+def add_message(new_message):
+    old_history = st.session_state.history
+    st.session_state.history = f'{new_message}\n{old_history}'
+
+def text_entered():
+    add_message(f'User: {st.session_state.input}')
+    if st.session_state.history.startswith('User'):
+        st.session_state.message = st.session_state.input
+    st.session_state.input = ''
+
+if st.session_state.pending:
+    reply = generate_reply(st.session_state.message)
+    add_message(f'Bot: {reply}')
+    st.session_state.message = None
+    st.session_state.pending = False
+
+st.text_input(
+    'Input:',
+    key='input',
+    on_change=text_entered,
+    placeholder='Hello bot',
+)
+
+st.text_area(
+    'Chat:',
+    key='history',
+    disabled=True,
+)
+
+if st.session_state.message:
+    st.session_state.pending = True
+    time.sleep(0.5)
+    st.experimental_rerun()
